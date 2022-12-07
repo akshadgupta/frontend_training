7dbe52842bb9db7920fbe881d481612b3f55c7b0
Akshad Gupta <akshad.gupta@grazitti.com>
2022-12-07 09:53:48 +0530

    commit

--- pages/Homepage/Homepage.js
+++ pages/Homepage/Homepage.js
@@ -1,12 +1,10 @@
 import Navbar from '../navbar/navbar';
 import  Sidebar from '../sidebar/sidebar';
-import Feed from '../feed/feed';
 export default function Homepage({  }) {
     return (
       <div>
         <Navbar/>
         <Sidebar/>
-        <Feed/>
       </div>
     );
 }
